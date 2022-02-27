1261d6a296cc890cf218db3f4cede82b0c84a611
CoelhoRafael <rafael.coelho@bandtec.com.br>
2022-02-27 20:49:43 -0300

    iniciando projeto spring

---
+++ sweet-stock-api/src/main/java/com/stock/sweet/sweetstockapi/SweetStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.stock.sweet.sweetstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetStockApiApplication.class, args);
+	}
+
+}
---
+++ sweet-stock-api/src/test/java/com/stock/sweet/sweetstockapi/SweetStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.sweet.sweetstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
