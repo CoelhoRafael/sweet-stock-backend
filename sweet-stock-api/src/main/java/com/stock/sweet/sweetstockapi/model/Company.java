ee5bee3f4821f124e640e2e427aef57afbe96ddb
Ricardo Galdino <79323687+Ricardobrito1@users.noreply.github.com>
2022-03-26 15:54:01 -0300

    Created Company model

---
+++ sweet-stock-api/src/main/java/com/stock/sweet/sweetstockapi/model/Company.java
@@ -0,0 +1,48 @@
+package com.stock.sweet.sweetstockapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "company")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "uuid")
+    private String uuid;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "fantansy_name")
+    private String fantansyName;
+
+    @Column(name = "ceo")
+    private String ceo;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telephone_number")
+    private String telephoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_address", referencedColumnName = "id")
+    private Address address;
+}
+
