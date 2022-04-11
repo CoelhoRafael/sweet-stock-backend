b495209643c40c5338fe44b20930f1c2ddb04aac
CoelhoRafael <rafael.coelho@bandtec.com.br>
2022-04-11 00:05:59 -0300

    refactor: retira imports desnessarios do fluxo

--- sweet-stock-api/src/main/java/com/stock/sweet/sweetstockapi/controller/IngredientReportController.java
+++ sweet-stock-api/src/main/java/com/stock/sweet/sweetstockapi/controller/IngredientReportController.java
@@ -2,16 +2,11 @@ package com.stock.sweet.sweetstockapi.controller;
 
 import com.stock.sweet.sweetstockapi.dto.request.IngredientReportRequest;
 import com.stock.sweet.sweetstockapi.mapper.IngredientReportMapper;
-import com.stock.sweet.sweetstockapi.model.Ingredient;
-import com.stock.sweet.sweetstockapi.repository.IngredientRepository;
 import com.stock.sweet.sweetstockapi.service.IngredientReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("ingredients-reports")
