e81fb60ea2e8d7752abbbf602848af5aca00b6a9
abnerlucss <abner.santos@bandtec.com.br>
2022-05-16 18:08:17 -0300

    feat: enviando html para o funcionário

--- src/main/java/com/stock/sweet/sweetstockapi/service/AccessService.java
+++ src/main/java/com/stock/sweet/sweetstockapi/service/AccessService.java
@@ -1,35 +1,12 @@
 package com.stock.sweet.sweetstockapi.service;
 
+import com.stock.sweet.sweetstockapi.exception.NotFoundException;
 import com.stock.sweet.sweetstockapi.model.Company;
-import com.stock.sweet.sweetstockapi.model.Email;
 import com.stock.sweet.sweetstockapi.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
 public class AccessService {
-    @Autowired
-    private EmailService emailService;
 
-    @Autowired
-    private CompanyRepository companyRepository;
-
-    public void sendInvite(String email, String uuidCompany) {
-        Company company = companyRepository.findByUuid(uuidCompany).orElse(null);
-
-        if (company != null) {
-            emailService.sendEmail(
-                    new Email(
-                            "sweet-stock",
-                            "rafaelcoelho3110@gmail.com",
-                            email,
-                            "Token para cadastro de funcionário - SWEET-STOCK",
-                            company.getAssociateCode(),
-                            LocalDateTime.now()
-                    )
-            );
-        }
-    }
 }
--- src/main/java/com/stock/sweet/sweetstockapi/service/EmailService.java
+++ src/main/java/com/stock/sweet/sweetstockapi/service/EmailService.java
@@ -1,18 +1,35 @@
 package com.stock.sweet.sweetstockapi.service;
 
+import com.stock.sweet.sweetstockapi.config.TemplateEngineConfig;
 import com.stock.sweet.sweetstockapi.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
+
+    @Autowired
+    private TemplateEngineConfig templateEngineConfig;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private TemplateEngine htmlTemplateEngine;
+
 
     public void sendEmail(Email emailModel) {
         emailModel.setSendDateEmail(LocalDateTime.now());
@@ -28,4 +45,29 @@ public class EmailService {
             System.out.println("Falha ao enviar email" + e);
         }
     }
+
+    public boolean sendHtmlEmail(String to, String subject, String template, Context ctx) {
+
+        final MimeMessage mimeMessage = this.emailSender.createMimeMessage();
+        final MimeMessageHelper email;
+
+        try {
+            email = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+            email.setFrom("sweet.stock.contato@gmail.com");
+            email.setTo(to);
+            email.setSubject(subject);
+
+            final String htmlContent = this.htmlTemplateEngine.process(template, ctx);
+
+            email.setText(htmlContent, true);
+
+            emailSender.send(mimeMessage);
+
+            return true;
+        } catch (MailException | MessagingException e) {
+            return false;
+        }
+    }
+
 }
