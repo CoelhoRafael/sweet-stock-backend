6336bba84aa53f383d331aa9a93d996c5fb2284c
abnerlucss <abner.santos@bandtec.com.br>
2022-05-19 18:51:13 -0300

    feat: resolvendo bug do switch case

--- src/main/java/com/stock/sweet/sweetstockapi/mapper/DashboardMapper.java
+++ src/main/java/com/stock/sweet/sweetstockapi/mapper/DashboardMapper.java
@@ -102,26 +102,48 @@ public class DashboardMapper {
         switch (date.getMonth().getValue()) {
             case 1:
                 month = "Janeiro";
+                break;
+
             case 2:
                 month = "Fevereiro";
+                break;
+
             case 3:
                 month = "Março";
+                break;
+
             case 4:
                 month = "Abril";
+                break;
+
             case 5:
                 month = "Maio";
+                break;
+
             case 6:
                 month = "Junho";
+                break;
+
             case 7:
                 month = "Julho";
+                break;
+
             case 8:
                 month = "Agosto";
+                break;
+
             case 9:
                 month = "Setembro";
+                break;
+
             case 10:
                 month = "Outubro";
+                break;
+
             case 11:
                 month = "Novembro";
+                break;
+
             case 12:
                 month = "Dezembro";
                 break;
