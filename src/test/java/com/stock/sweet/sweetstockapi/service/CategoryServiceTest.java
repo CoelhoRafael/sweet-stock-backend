6535874b31a736b9fce0102e2477e0e0e3dfcd9b
Ricardo Galdino <ricardo.brito@bandtec.com.br>
2022-05-20 17:57:40 -0300

    Test

---
+++ src/test/java/com/stock/sweet/sweetstockapi/service/CategoryServiceTest.java
@@ -0,0 +1,28 @@
+package com.stock.sweet.sweetstockapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceTest {
+
+    @Test
+    void createCategory() {
+    }
+
+    @Test
+    void getAllCategories() {
+    }
+
+    @Test
+    void findCategoryByUuid() {
+    }
+
+    @Test
+    void updateCategory() {
+    }
+
+    @Test
+    void deleteCategory() {
+    }
+}
---
+++ src/test/java/com/stock/sweet/sweetstockapi/service/ProviderServiceTest.java
@@ -0,0 +1,28 @@
+package com.stock.sweet.sweetstockapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProviderServiceTest {
+
+    @Test
+    void createProvider() {
+    }
+
+    @Test
+    void getAllProviders() {
+    }
+
+    @Test
+    void findProviderByUuid() {
+    }
+
+    @Test
+    void updateProvider() {
+    }
+
+    @Test
+    void deleteProvider() {
+    }
+}
