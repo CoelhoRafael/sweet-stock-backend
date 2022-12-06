894fc52fcfead65c8ed13676b07fdbf53bf34984
CoelhoRafael <rafaelcoelho3110@gmail.com>
2022-12-05 23:01:02 -0300

    fix: arrumando order

--- src/main/java/com/stock/sweet/sweetstockapi/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/stock/sweet/sweetstockapi/exception/ApplicationExceptionHandler.java
@@ -7,25 +7,25 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@Slf4j
-//@ControllerAdvice
-//public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity handleException(Exception exception){
-//        log.info("Recebendo tratativa de exception no metodo handleException -> Exception");
-//
-//        DefaultError defaultError = new DefaultError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
-//
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(defaultError);
-//    }
-//
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public ResponseEntity handleException(IllegalArgumentException exception){
-//        log.info("Recebendo tratativa de exception no metodo handleException -> IllegalArgumentException");
-//
-//        DefaultError defaultError = new DefaultError(422, exception.getMessage());
-//
-//        return ResponseEntity.status(422).body(defaultError);
-//    }
-//}
+@Slf4j
+@ControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception exception){
+        log.info("Recebendo tratativa de exception no metodo handleException -> Exception");
+
+        DefaultError defaultError = new DefaultError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(defaultError);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleException(IllegalArgumentException exception){
+        log.info("Recebendo tratativa de exception no metodo handleException -> IllegalArgumentException");
+
+        DefaultError defaultError = new DefaultError(422, exception.getMessage());
+
+        return ResponseEntity.status(422).body(defaultError);
+    }
+}
