31e99132f22376e89c0a790ec461501f1f9b2611
CoelhoRafael <rafaelcoelho3110@gmail.com>
2023-01-22 18:42:05 -0300

    fix: ajustes aws

---
+++ src/main/java/com/stock/sweet/sweetstockapi/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.stock.sweet.sweetstockapi.controller;
+
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@CrossOrigin(origins = "*")
+@SecurityRequirement(name = "bearerAuth")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("Api sweet store funcionando corretamente!");
+    }
+}
